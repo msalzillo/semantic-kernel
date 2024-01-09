c6036c7d7346410126a0e15ca5ae8794127c6050
Michael Salzillo <mjsalzillo@gmail.com>
2024-01-09 18:02:39 -0500

    Chat updates

--- dotnet/chatapi/Services/SemanticKernelService.cs
+++ dotnet/chatapi/Services/SemanticKernelService.cs
@@ -18,7 +18,9 @@ public class SemanticKernelService : ISemanticKernelService
 {
     private const string Endpoint = "https://grafedi-openai.openai.azure.com/";
     private const string ApiKey = "b993eb427a574b859e1d77adc6c4ba0e";
-    private const string Deployment = "grafedi-openai-tester";
+    // private const string Deployment = "grafedi-openai-tester";
+    private const string Deployment = "grafedi-16k";
+    // private const string Deployment = "grafedi-instruct";
     private const string DeploymentEmbedding = "grafedi-embedding";
 
     private const string PineconeEnvironment = "eastus-azure";

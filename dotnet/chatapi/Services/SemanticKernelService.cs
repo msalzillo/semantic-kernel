80f1ab2bb532808b9f12a99b780dad3d2c3b9128
Michael Salzillo <mjsalzillo@gmail.com>
2024-01-11 08:36:54 -0500

    Removed access information

--- dotnet/chatapi/Services/SemanticKernelService.cs
+++ dotnet/chatapi/Services/SemanticKernelService.cs
@@ -16,18 +16,6 @@ public interface ISemanticKernelService
 
 public class SemanticKernelService : ISemanticKernelService
 {
-    private const string Endpoint = "https://grafedi-openai.openai.azure.com/";
-    private const string ApiKey = "b993eb427a574b859e1d77adc6c4ba0e";
-    // private const string Deployment = "grafedi-openai-tester";
-    private const string Deployment = "grafedi-16k";
-    // private const string Deployment = "grafedi-instruct";
-    private const string DeploymentEmbedding = "grafedi-embedding";
-
-    private const string PineconeEnvironment = "eastus-azure";
-    private const string PineconeEnvironmentKey = "68ba380b-dcce-416f-ab9d-39126267bd84";
-
-    private const string EmbeddingModel = "text-embedding-ada-002";
-
     private readonly Kernel _kernel;
     private readonly PineconeMemoryStore _pineconeMemoryStore;
     private readonly ISemanticTextMemory _memory;
